a78dbe8d95703ae06df613a3a9a4406bc0289ba2
mcpar-land <john@mcpar.land>
2023-12-20 13:58:39 -0500

    handle missing params

--- add_ip_to_db_staging.sh
+++ add_ip_to_db_staging.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo adding $1 to firewall for db-1-staging. get your password in your clipboard, genius
-ssh -t jmcparland@162.216.17.5 sudo -- "sh -c 'ufw allow from $1 to any port 3306 && ufw reload'"
+
+IP=${1:?missing ip address}
+
+echo adding $IP to firewall for db-1-staging. get your password in your clipboard, genius
+ssh -t jmcparland@162.216.17.5 sudo -- "sh -c 'ufw allow from $IP to any port 3306 && ufw reload'"
--- clear_staging_cache.sh
+++ clear_staging_cache.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-echo sshing in to clear the staging cache for tenant $1. get your password in your clipboard, genius
-ssh -t jmcparland@104.237.151.215 "sudo /var/www/elstats/app/Console/cake cache $1 clear"
+
+TENANT=${1:?missing tenant id}
+
+echo sshing in to clear the staging cache for tenant $TENANT. get your password in your clipboard, genius
+ssh -t jmcparland@104.237.151.215 "sudo /var/www/elstats/app/Console/cake cache $TENANT clear"
